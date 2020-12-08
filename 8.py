c8f0342379c35b5e7b00ed90ecabc74b6511a302
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-12-08 11:54:53 +0100

    Move used boolean to separate list to avoid unnecessary copies

--- 8.py
+++ 8.py
@@ -4,11 +4,15 @@ import sys
 data = [row.split() for row in open('8.txt').read().split('\n') if row]
 data = [[row[0], int(row[1]), False] for row in data]
 
+# part 1
+
 i = 0
 acc = 0
 
-while not data[i][2]:
-    data[i][2] = True
+used = [False]*len(data)
+        
+while not used[i]:
+    used[i] = True
     if data[i][0] == 'acc':
         acc += data[i][1]
         i += 1
@@ -19,13 +23,11 @@ while not data[i][2]:
 
 print(acc)
 
-i = 0
-acc = 0
-
-def test_change(i, acc, orig_data):
-    data = copy.deepcopy(orig_data)
-    while i < len(data) and not data[i][2]:
-        data[i][2] = True
+# part 2
+def test_change(i, acc, data, used):
+    used = used[:]
+    while i < len(data) and not used[i]:
+        used[i] = True
         if data[i][0] == 'acc':
             acc += data[i][1]
             i += 1
@@ -37,24 +39,27 @@ def test_change(i, acc, orig_data):
         return True, acc
     return False, -1
 
+
+i = 0
+acc = 0
+used = [False]*len(data)
+
 while i < len(data):
-    data[i][2] = True
+    used[i] = True
     if data[i][0] == 'acc':
         acc += data[i][1]
         i += 1
         continue
     elif data[i][0] == 'jmp':
-        res = test_change(i+1, acc, data)
+        res = test_change(i+1, acc, data, used)
         if res[0]:
             print(res[1])
             sys.exit(0)
         i += data[i][1]
     else:
-        res = test_change(i+data[i][1], acc, data)
+        res = test_change(i+data[i][1], acc, data, used)
         if res[0]:
             print(res[1])
             sys.exit(0)
         i += 1
 
-
-print('fail')
