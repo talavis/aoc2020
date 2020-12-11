5e0fc5bb8f3d7ad78b69cc9b1c44838951bf321f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-12-11 09:51:37 +0100

    Day 11 part 2

--- 11.py
+++ 11.py
@@ -1,11 +1,11 @@
 import copy
+import time
 import itertools
 
 def part1(data):
     def step(data):
         new_data = copy.deepcopy(data)
-        # first row:
-
+        # corners
         for entry in ((0, 0), (0, -1), (-1, 0), (-1, -1)):
             if data[entry[0]][entry[1]] != '.':
                 new_data[entry[0]][entry[1]] = '#'
@@ -66,98 +66,124 @@ def part1(data):
         return first == second
 
     old_data = [['asd']]
-    i = 0
     while not same(data, old_data):
-        i += 1
         old_data = data
         data = step(data)
- 
+
     taken_seats = 0
     for row in data:
         taken_seats += row.count('#')
-    print('\n'.join([''.join(row) for row in data]))
-    print()
     return taken_seats
-    
+
+############################################################
+
 
 def part2(data):
+    def find_neighbours(r, c):
+        nearby = []
+        for direction in ((1,1), (1, -1), (-1, 1), (-1, -1),
+                          (1, 0), (-1, 0), (0, 1), (0, -1)):
+            if (0 <= r + direction[0] < len(data) and
+                0 <= c + direction[1] < len(data[0])):
+                i = r + direction[0]
+                j = c + direction[1]
+            else:
+                continue
+
+            while (0 <= i + direction[0] < len(data) and
+                   0 <= j + direction[1] < len(data[0]) and
+                   data[i][j] == '.'):
+                i += direction[0]
+                j += direction[1]
+            if not (i == r and j == c):
+                nearby.append((i, j))
+        return nearby
+
+
+    def find_nearby(r, c):
+        return [data[entry[0]][entry[1]] for entry in neighbours[(r, c)]]
+
+
     def step(data):
         new_data = copy.deepcopy(data)
-        # first row:
-
+        # corners:
         for entry in ((0, 0), (0, -1), (-1, 0), (-1, -1)):
             if data[entry[0]][entry[1]] != '.':
                 new_data[entry[0]][entry[1]] = '#'
 
+        # first row:
         for pos in range(1, len(data[0])-1):
             if data[0][pos] != '.':
-                nearby = data[0][pos-1:pos+2] + data[1][pos-1:pos+2]
+                nearby = find_nearby(0, pos)
                 if data[0][pos] == 'L' and not nearby.count('#'):
                     new_data[0][pos] = '#'
                 elif data[0][pos] == '#':
-                    if nearby.count('#') >= 5:  # since the seat itself is included
+                    if nearby.count('#') >= 5:
                         new_data[0][pos] = 'L'
 
         # last row
         for pos in range(1, len(data[0])-1):
             if data[-1][pos] != '.':
-                nearby = data[-1][pos-1:pos+2] + data[-2][pos-1:pos+2]
+                nearby = find_nearby(len(data)-1, pos)
                 if data[-1][pos] == 'L' and not nearby.count('#'):
                     new_data[-1][pos] = '#'
                 elif data[-1][pos] == '#':
-                    if nearby.count('#') >= 5:  # since the seat itself is included
+                    if nearby.count('#') >= 5:
                         new_data[-1][pos] = 'L'
         # first col
         for row in range(1, len(data)-1):
             if data[row][0] != '.':
-                nearby = data[row][1:2] + data[row-1][0:2] + data[row+1][0:2]
+                nearby = find_nearby(row, 0)
                 if data[row][0] == 'L' and not nearby.count('#'):
                     new_data[row][0] = '#'
                 elif data[row][0] == '#':
-                    if nearby.count('#') >= 4:
+                    if nearby.count('#') >= 5:
                         new_data[row][0] = 'L'
         # last col
         for row in range(1, len(data)-1):
             if data[row][-1] != '.':
-                nearby = (data[row][-2] +
-                          data[row-1][-2] + data[row-1][-1] +
-                          data[row+1][-2] + data[row+1][-1])
+                nearby = find_nearby(row, len(data[0])-1)
                 if data[row][-1] == 'L' and not nearby.count('#'):
                     new_data[row][-1] = '#'
                 elif data[row][-1] == '#':
-                    if nearby.count('#') >= 4:
+                    if nearby.count('#') >= 5:
                         new_data[row][-1] = 'L'
         # all center fields
         for i in range(1,len(data)-1):
             for j in range(1, len(data[0])-1):
                 if data[i][j] != '.':
-                    nearby = list(itertools.chain.from_iterable(row[j-1:j+2] for row in data[i-1:i+2]))
+                    nearby = find_nearby(i, j)
                     if data[i][j] == 'L' and not nearby.count('#'):
                         new_data[i][j] = '#'
                     elif data[i][j] == '#':
-                        if nearby.count('#') >= 5:  # since the seat itself is included
+                        if nearby.count('#') >= 5:
                             new_data[i][j] = 'L'
         return new_data
         
+
     def same(first, second):
         first = ''.join(''.join(row) for row in first)
         second = ''.join(''.join(row) for row in second)
         return first == second
 
+    neighbours = {}
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            neighbours[(i, j)] = find_neighbours(i, j)
+
     old_data = [['asd']]
-    i = 0
     while not same(data, old_data):
-        i += 1
         old_data = data
         data = step(data)
- 
     taken_seats = 0
     for row in data:
         taken_seats += row.count('#')
-    print('\n'.join([''.join(row) for row in data]))
-    print()
+#    print('\n'.join([''.join(row) for row in data]))
+#    print()
     return taken_seats
 
+
+############################################################
 
 data = [list(val) for val in open('11.txt').read().split('\n') if val]
 
@@ -187,5 +213,16 @@ L.LLLLLLLL
 #print(part1(test_data2))
 
 #print(data)
-print(part1(data))
+start = time.time()
+res = part1(data)
+stop = time.time()
+print(stop-start)
+print(res)
+#print(part2(test_data))
 
+#print(len(data), len(data[0]))
+start = time.time()
+res = part2(data)
+stop = time.time()
+print(stop-start)
+print(res)
